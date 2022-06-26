60cbf6ad7f0ab05f25aa3537768545265e5d8e03
KaixiangChris <45470830+KaixiangChris@users.noreply.github.com>
2022-06-26 18:36:30 -0400

    Update MainActivity.java

--- GameSuite/app/src/main/java/com/example/gamesuite/MainActivity.java
+++ GameSuite/app/src/main/java/com/example/gamesuite/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Kaixiang Cui
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
